f2d2e78df843d412d2f8af77a14e42ac17e7541a
dyuangwim <dyuangwim@gmail.com>
2025-08-07 08:34:03 +0800

    Update read_rfid.py

--- read_rfid.py
+++ read_rfid.py
@@ -2,7 +2,7 @@ from mfrc522 import MFRC522
 import RPi.GPIO as GPIO
 
 GPIO.setwarnings(False)
-reader = MFRC522.MFRC522()
+reader = MFRC522()
 
 try:
     print("请将 RFID 标签靠近读取器...")
@@ -18,18 +18,17 @@ try:
                 uid_str = ''.join(str(i) for i in uid)
                 print("标签 ID:", uid_str)
 
-                # 验证 sector 1 block 4
-                block = 4
+                block = 4  # 你要读取的区块编号
+                key = [0xFF] * 6  # 默认 key
                 reader.MFRC522_SelectTag(uid)
-                key = [0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF]  # 默认 key
-
                 auth = reader.MFRC522_Auth(reader.PICC_AUTHENT1A, block, key, uid)
+
                 if auth == reader.MI_OK:
                     data = reader.MFRC522_Read(block)
-                    print("Block 4 内容:", data)
+                    print("✅ Block 4 内容:", data)
                     reader.MFRC522_StopCrypto1()
                 else:
-                    print("❌ 无法认证 Block 4")
+                    print("❌ 无法认证 block")
                 break
 finally:
     GPIO.cleanup()
