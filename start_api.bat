1f876e911cf1210079f4119b24077930674f6024
dyuangwim <dyuangwim@gmail.com>
2025-09-24 12:01:24 +0800

    Update start_api.bat

--- start_api.bat
+++ start_api.bat
@@ -2,3 +2,8 @@
 cd /d "C:\Users\dyuan\Downloads\GWIM_RFID Warehouse Management System"
 call venv\Scripts\activate
 python -m uvicorn main:app --host 0.0.0.0 --port 8080
+
+@echo off
+cd /d C:\rfid_api
+call .\.venv\Scripts\activate
+uvicorn main:app --host 0.0.0.0 --port 8080
